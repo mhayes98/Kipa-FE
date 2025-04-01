f68a0fe38dfe04d30941be006eabab116bbc0dad
matt <mh.dev@pm.me>
2025-03-31 21:18:38 -0400

    AlbumService API calls created

---
+++ kipa-fe/services/AlbumServices.js
@@ -0,0 +1,14 @@
+// POST
+export const addAlbumToDatabase = async () => {}
+
+// POST
+export const createUserAlbumConnection = async () => {}
+
+// POST
+export const updateUserAlbumStatus = async () => {}
+
+// PUT
+export const updateUserAlbumTags = async () => {}
+
+// PUT
+export const updateUserAlbumNotes = async () => {}
