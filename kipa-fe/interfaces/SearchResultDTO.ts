40769412470d9c5fc56d0e1c068f98b27a3ffde3
matt <mh.dev@pm.me>
2025-04-07 21:36:18 -0400

    Search API DTO created

---
+++ kipa-fe/interfaces/SearchResultDTO.ts
@@ -0,0 +1,14 @@
+interface MasterDTO {
+    title: string;
+    thumb: string;
+    genre: string;
+    style: string;
+    year: string;
+}
+
+interface ArtistDTO {
+    title: string;
+    thumb: string;
+    genre: string
+    style: string;
+}
