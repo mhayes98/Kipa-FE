5fefa4dfef538fdbd2e68e10e7c523e1872ffa04
matt <mh.dev@pm.me>
2025-04-17 00:12:49 -0400

    Removed testing output & corrected onclick functionality

--- kipa-fe/components/Button/WantButton.jsx
+++ kipa-fe/components/Button/WantButton.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { useLoginModalContext } from "../../context/LoginModalContext";
 import { useUserAuthContext } from "../../context/UserAuthContext";
+import { getTracklistByReleaseID, addAlbumToDatabase } from '../../services/AlbumServices';
 
 // CLICKING THIS BUTTON WILL NEED TO TRIGER A LOGIN IF USER IS NOT AUTH
 
 function WantButton(master) {
     const { username } = useUserAuthContext();
 
-
-    async function placeholder(id, username) {
-        console.log("PLACEHOLDER --> WANT");
-    }
-
     return (
         <>
-            <button onClick={placeholder(master.id, username)}>Own</button>
+        <button onClick={() => addAlbumToDatabase(master)}>Want</button>
         </>
     )
 }
