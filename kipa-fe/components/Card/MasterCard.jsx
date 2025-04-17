31522706e64d7640c50bc4edb863302dc77a7e47
matt <mh.dev@pm.me>
2025-04-17 00:12:24 -0400

    Removed testing output

--- kipa-fe/components/Card/MasterCard.jsx
+++ kipa-fe/components/Card/MasterCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { WantButton } from '../Button/WantButton';
 
 function MasterCard(master) {
-    console.log("TESTING MASTERCARD:" + master.artist);
     return (
         <>
             <h1>{master.artist}</h1>
             <img src={master.thumb}/>
+            <WantButton{...master}/>
             <h4>{master.title} - {master.year}</h4>
         </>
     );
