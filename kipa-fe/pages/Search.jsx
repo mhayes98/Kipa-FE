7d36574f764021eb104084932e2115f6b500bf16
matt <mh.dev@pm.me>
2025-04-25 18:56:25 -0400

    Search wrapped in providers

--- kipa-fe/pages/Search.jsx
+++ kipa-fe/pages/Search.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { UserAuthContextProvider } from "../context/UserAuthContext";
 import SearchBar from "../components/Bar/SearchBar";
+import { LoginModalContextProvider } from "../context/LoginModalContext";
+import { LoginModal } from "../components/Modal/LoginModal";
 
 function Search() {
     return( 
         <>
             <UserAuthContextProvider>
-                <h1>Search</h1>
-                <SearchBar/>
+                <LoginModalContextProvider>
+                    <h1>Search</h1>
+                    <SearchBar/>
+                    <LoginModal/>
+                </LoginModalContextProvider>
             </UserAuthContextProvider>
         </>
     )
