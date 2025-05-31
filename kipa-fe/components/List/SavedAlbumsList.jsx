d68808cd4556bdfc877be4d4def21cd6da5fcd4b
matt <mh.dev@pm.me>
2025-05-31 11:34:35 -0400

    Added more testing

--- kipa-fe/components/List/SavedAlbumsList.jsx
+++ kipa-fe/components/List/SavedAlbumsList.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useUserAuthContext } from "../../context/UserAuthContext";
-import { getMySavedAlbums } from '../../services/AlbumServices.js';
+import { getMySavedAlbums } from '../../services/AlbumServices';
 import { useLoginModalContext } from "../../context/LoginModalContext";
 
 function SavedAlbumList() {
-    const { authenticated, username } = useUserAuthContext();
+    const { username } = useUserAuthContext();
+    const { isAuthenticated } = useUserAuthContext();
     const { toggleVisibility } = useLoginModalContext();
 
     // Authenticated check not working correctly
     // Username passthrough not working correctly
-    if(authenticated){
+    if(isAuthenticated){
+        console.log("AUTH");
         getMySavedAlbums(username);
     }
     else {
+        console.log(isAuthenticated);
         toggleVisibility;
     }
+
     // Manuaal call to 'testuser' is functional
-    getMySavedAlbums("testuser");
+    //getMySavedAlbums("testuser");
 
     return(
         <>
