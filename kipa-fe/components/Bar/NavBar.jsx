f7ddfda773e3df6d11f1490d1f1ae7afe72fc10e
matt <mh.dev@pm.me>
2025-06-01 22:17:40 -0400

    Navigation bar created

---
+++ kipa-fe/components/Bar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+function NavBar() {
+
+    return (
+        <>
+
+        <nav>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
+                Home
+            </NavLink>
+            <NavLink to="/search" className={({ isActive }) => isActive ? "active" : ""}>
+                Search
+            </NavLink>
+            <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>
+                About
+            </NavLink>
+             <NavLink to="/register" className={({ isActive }) => isActive ? "active" : ""}>
+                Register
+            </NavLink>
+        </nav>
+        </>
+    );
+}
+
+export default NavBar;
