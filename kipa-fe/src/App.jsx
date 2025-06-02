6211a60e1a8f0a4c20a733c3fe1cc2906a5067fe
matt <mh.dev@pm.me>
2025-06-01 22:19:16 -0400

    Added navbar to global app

--- kipa-fe/src/App.jsx
+++ kipa-fe/src/App.jsx
@@ -1,5 +1,6 @@
 import { LoginModal } from '../components/Modal/LoginModal.jsx';
 import { LoginButton } from '../components/Button/LoginButton.jsx';
+import NavBar from '../components/Bar/NavBar.jsx';
 import { LoginModalContextProvider  } from '../context/LoginModalContext.jsx';
 import { UserAuthContextProvider } from '../context/UserAuthContext.jsx';
 import { Outlet } from "react-router-dom";
@@ -8,6 +9,7 @@ function App() {
   return (
     <UserAuthContextProvider>
       <LoginModalContextProvider>
+        <NavBar />
         <LoginButton />
         <LoginModal />
         <Outlet />
