7ae2e9e8b868b20eef3d8316bb1d262b25d41a48
matt <mh.dev@pm.me>
2025-06-01 22:18:16 -0400

    Corrected list creation logic

--- kipa-fe/components/List/SavedAlbumsList.jsx
+++ kipa-fe/components/List/SavedAlbumsList.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import { useUserAuthContext } from "../../context/UserAuthContext";
-import { getMySavedAlbums } from '../../services/AlbumServices';
+import { getMySavedAlbums, processMySavedAlbumsResponse } from '../../services/AlbumServices';
 import { useLoginModalContext } from "../../context/LoginModalContext";
+import { SavedAlbumCard } from '../Card/SavedAlbumCard';
 
 function SavedAlbumList() {
-    const { username } = useUserAuthContext();
-    const { isAuthenticated } = useUserAuthContext();
+    const { username, authenticated } = useUserAuthContext();
     const { toggleVisibility } = useLoginModalContext();
 
-    // Authenticated check not working correctly
-    // Username passthrough not working correctly
-    if(isAuthenticated){
-        console.log("AUTH");
-        getMySavedAlbums(username);
+    if(authenticated){
+        console.log(`Authenticated = True. Username - ${username}`);
+        processMySavedAlbumsResponse(username);
     }
     else {
-        console.log(isAuthenticated);
-        toggleVisibility;
+        console.log("Not authenticated");
+        //toggleVisibility();
     }
 
-    // Manuaal call to 'testuser' is functional
-    //getMySavedAlbums("testuser");
+    /*  
+        Need to figure out how we want this page to look/work
+            - How do we want a non-authenticated page to look?
+                - Login screen? Modal? Do we want to bother with that now or later?
+            - I think we need a higher level component that directs the page content based on auth state
+                - render login/registration content if not auth
+                - render list/card content if auth
+                - Decision is based on the above question - need to decide before proceeding.
+    */
 
     return(
         <>
+            <div className="grid grid-cols-3 divide-x-3 divide-dashed m-4">
+                {results.map((result, index) => (
+                    <SavedAlbumCard key={index} master={result} />
+                ))}
+            </div>
         </>
     )
 }
