da70e5803f80b920f5bbefabede703101dad5766
matt <mh.dev@pm.me>
2025-06-05 01:30:00 -0400

    Added auth request view

--- kipa-fe/pages/Home.jsx
+++ kipa-fe/pages/Home.jsx
@@ -3,12 +3,14 @@ import { LoginModal } from '../components/Modal/LoginModal';
 import { LoginModalContextProvider  } from '../context/LoginModalContext';
 import { UserAuthContextProvider } from "../context/UserAuthContext";
 import { SavedAlbumList } from '../components/List/SavedAlbumsList';
+import AuthPromptView from "../components/View/AuthPromptView";
 
 function Home() {
     return (
         <>
             <h1>Home</h1>
             <SavedAlbumList />
+            <AuthPromptView />
             <LoginModal />
         </>
     );
