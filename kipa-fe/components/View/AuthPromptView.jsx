79d9dbd2c677f1824b24c17d463c9e189df16d99
matt <mh.dev@pm.me>
2025-06-05 01:30:43 -0400

    Created view component to prompt user to login or register

---
+++ kipa-fe/components/View/AuthPromptView.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AuthPromptView() {
+
+
+    return (
+        <>
+            <h1>You need to be authenticated.</h1>
+            <br></br>
+            <h1>Login now, or Register.</h1>
+        </>
+    )
+}
+
+export default AuthPromptView;
