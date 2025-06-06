43102f4c424129ac95d6ef604834a68e6661396f
matt <mh.dev@pm.me>
2025-06-05 23:29:14 -0400

    Implemented card functionality - WIP

--- kipa-fe/components/Card/SavedAlbumCard.jsx
+++ kipa-fe/components/Card/SavedAlbumCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 
-function SavedAlbumCard() {
+function SavedAlbumCard({ album }) {
+    const { albumAlbumID, genre, notes, status,
+            style, tags, thumbnail, title, tracklist, year } = album;
+    
+    // Need to add the logic to cycle through all styles, genres, tags, etc..
+    // Determine how tracklist will be implemented --> AlbumModal most likely? More details?
     return (
         <>
-
+            <div className="border-2 border-solid">
+                <h1>{title}</h1>
+                <img src={thumbnail}/>
+                <p>{year}</p>
+                <p>{tags}</p>
+                <p>{notes}</p>
+            </div>
         </>
     )
 }
