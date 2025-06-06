cae5675890741aed948c1b959d43dfe69271d53d
matt <mh.dev@pm.me>
2025-06-05 23:30:38 -0400

    Added logic to render components based on auth state

--- kipa-fe/pages/Home.jsx
+++ kipa-fe/pages/Home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { LoginModal } from '../components/Modal/LoginModal';
 import { LoginModalContextProvider  } from '../context/LoginModalContext';
-import { UserAuthContextProvider } from "../context/UserAuthContext";
+import { UserAuthContextProvider, useUserAuthContext } from "../context/UserAuthContext";
 import { SavedAlbumList } from '../components/List/SavedAlbumsList';
 import AuthPromptView from "../components/View/AuthPromptView";
+import { processMySavedAlbumsResponse } from "../services/AlbumServices";
 
 function Home() {
+    const { authenticated, username } = useUserAuthContext();
+
+
     return (
         <>
-            <h1>Home</h1>
-            <SavedAlbumList />
-            <AuthPromptView />
-            <LoginModal />
+            <div>
+            {authenticated && (
+                <SavedAlbumList />
+            )}
+            </div>
+            {!authenticated && (
+                <AuthPromptView />
+            )}
         </>
     );
 }
