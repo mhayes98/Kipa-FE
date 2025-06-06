1ebb24128a2b6760bbe9864c45492ffbf75cdebf
matt <mh.dev@pm.me>
2025-06-05 23:30:23 -0400

    Added functional login prompt & registration redirection

--- kipa-fe/components/View/AuthPromptView.jsx
+++ kipa-fe/components/View/AuthPromptView.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { useLoginModalContext } from "../../context/LoginModalContext";
+import { NavLink } from 'react-router';
 
 function AuthPromptView() {
-
+    const { toggleVisibility } = useLoginModalContext();
 
     return (
         <>
-            <h1>You need to be authenticated.</h1>
-            <br></br>
-            <h1>Login now, or Register.</h1>
+            <h2>Login to view your collection!</h2>
+            <button type="button"
+            onClick={toggleVisibility}
+            className="bg-blue-400 text-white px-1.5 py-0.2 rounded hover:bg-blue-600">
+                Login
+            </button>
+            <h2>Don't have an account? Click <NavLink to="/register" className="text-blue-500">here</NavLink> to register!</h2>
         </>
     )
 }
