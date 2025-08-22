4be41a4fe6c10a6a0d8c218883b23e36200dae06
matt <mh.dev@pm.me>
2025-08-21 22:31:46 -0400

    Created context for Album Modal

---
+++ kipa-fe/context/AlbumModalContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useContext } from "react";
+
+export const AlbumModalContext = createContext({
+    visibility: false,
+    toggleVisibility: () => {}
+});
+
+function AlbumModalContextProvider({ children }) {
+    const [visibility, setVisibility] = useState(false);
+    const [master, setMaster] = useState("");
+
+    const toggleVisibility = () => {
+        setVisibility(visibility => visibility ? false : true);
+    };
+
+    return (
+        <AlbumModalContext.Provider value={{ visibility, toggleVisibility, setMaster, master, setMasterState }}>
+            {children}
+        </AlbumModalContext.Provider>
+    );
+}
+
+function useAlbumModalContext() {
+    const context = useContext(AlbumModalContext);
+    if (!context) {
+        throw new Error("useAlbumModalContext must be used within a AlbumModalContextProvider")
+    }
+    return context
+}
+
+export { AlbumModalContextProvider, useAlbumModalContext };
