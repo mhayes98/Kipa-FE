33c6cb0059bd79da76132d24266fa06dbb0719a4
matt <mh.dev@pm.me>
2025-08-21 22:31:21 -0400

    Added Album Modal & Context

--- kipa-fe/pages/Search.jsx
+++ kipa-fe/pages/Search.jsx
@@ -3,13 +3,18 @@ import { UserAuthContextProvider } from "../context/UserAuthContext";
 import SearchBar from "../components/Bar/SearchBar";
 import { LoginModalContextProvider } from "../context/LoginModalContext";
 import { LoginModal } from "../components/Modal/LoginModal";
+import { AlbumModalContextProvider } from '../context/AlbumModalContext.jsx';
+import { AlbumModal } from "../components/Modal/AlbumModal";
 
 function Search() {
     return( 
         <>
-            <h1>Search</h1>
-            <SearchBar/>
-            <LoginModal/>
+            <AlbumModalContextProvider>
+                <h1>Search</h1>
+                <SearchBar/>
+                <LoginModal/>
+                <AlbumModal/>
+            </AlbumModalContextProvider>
         </>
     )
 }
