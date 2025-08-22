a88ddb7cc02af563ab8214fda3013335f579e606
matt <mh.dev@pm.me>
2025-08-22 00:49:45 -0400

    Cleaned formatting

--- kipa-fe/components/Bar/NavBar.jsx
+++ kipa-fe/components/Bar/NavBar.jsx
@@ -7,18 +7,13 @@ function NavBar() {
         <>
 
         <nav>
-            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
-                Home
-            </NavLink>
-            <NavLink to="/search" className={({ isActive }) => isActive ? "active" : ""}>
-                Search
-            </NavLink>
-            <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>
-                About
-            </NavLink>
-             <NavLink to="/register" className={({ isActive }) => isActive ? "active" : ""}>
-                Register
-            </NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
+
+            <NavLink to="/search" className={({ isActive }) => isActive ? "active" : ""}>Search</NavLink>
+
+            <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>About</NavLink>
+
+            <NavLink to="/register" className={({ isActive }) => isActive ? "active" : ""}>Register</NavLink>
         </nav>
         <p>____________________________________________________________</p>
         </>
