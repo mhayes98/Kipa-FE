4e24df38547c0d09d47f3674ddbfb41e38c31b8f
matt <mh.dev@pm.me>
2025-08-23 12:15:31 -0400

    Added placeholder values, corrected state values, and commented out modal closure on outside click

--- kipa-fe/components/Modal/AlbumModal.jsx
+++ kipa-fe/components/Modal/AlbumModal.jsx
@@ -1,12 +1,12 @@
-import Reach from 'react';
+import React, { useEffect, useRef, useState } from "react";
 // import { TagTextBox } from "../../components/TextBox";
 // import { Notes } from "../../components/TextBox";
 import { useAlbumModalContext } from "../../context/AlbumModalContext";
 
-function AlbumModal({ master }) {
+function AlbumModal() {
+    const { visibility, toggleVisibility, master } = useAlbumModalContext();
     // Destrcturing the object to access values directly
     const { artist, thumb, title, year, genre } = master;
-    const { visibility, toggleVisibility } = useAlbumModalContext();
 
 
     // Close modal on ESC
@@ -18,32 +18,45 @@ function AlbumModal({ master }) {
         return () => window.removeEventListener("keydown", handleEsc);
     }, [toggleVisibility]);
 
-    // Close modal when clicking outside
-    const handleClickOutside = (e) => {
-        if (modalRef.current && !modalRef.current.contains(e.target)) {
-            toggleVisibility();
-        }
-    };
+    // Close modal when clicking outside - Maybe not use? Make closing intentional via escape or close button?
+    // const handleClickOutside = (e) => {
+    //     if (modalRef.current && !modalRef.current.contains(e.target)) {
+    //         toggleVisibility();
+    //     }
+    // };
 
-    console.log("AlbumModalContext loaded");
+    console.log("AlbumModal triggered");
+
+    const styles = {
+        border: '3px black'
+    };
 
     return (
         <>
-            <div>
-                <h1>{artist}</h1>
-                <img src={thumb}/>
-                <h2>{title} - {year}</h2>
-                <h4>{genre}</h4>
-                {/* Tags will go here - Function needs to parse tags*/}
-                {/* <TagTextBox/> */}
-                <p>Placeholder - TagTextBox</p>
+            {visibility && (
+                <div style={{styles}}>
+                    <h1>{artist}</h1>
+                    <img src={thumb}/>
+                    <h2>{title} - {year}</h2>
+                    <h4>{genre}</h4>
+                    {/* Tags will go here - Function needs to parse tags*/}
+                    {/* <TagTextBox/> */}
+                    <p>Placeholder - TagTextBox</p>
 
-                {/* Notes go here */}
-                {/* <Notes/> */}
-                <p>Placeholder - Notes</p>
+                    {/* Notes go here */}
+                    {/* <Notes/> */}
+                    <p>Placeholder - Notes</p>
 
-                {/* Potentially track list? */}
-            </div>
+                    {/* Potentially track list? */}
+                    <p>Placeholder - Track List?</p>
+
+                    {/*Save button goes here */}
+                    <p>Placeholder - Save Button</p>
+
+                    {/*Close button goes here */}
+                    <p>Placeholder - Close Button</p>
+                </div>
+            )}
         </>
     )
 }
