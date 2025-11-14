59f6ab6a08f403efd8fb642aeb4bcce63abedbb9
matt <mh.dev@pm.me>
2025-11-13 23:33:39 -0500

    Added blueprint & comments

--- kipa-fe/components/Modal/SavedAlbumModal.jsx
+++ kipa-fe/components/Modal/SavedAlbumModal.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useUserAuthContext } from "../../context/UserAuthContext";
+import { useSavedAlbumModalContext } from "../../context/SavedAlbumModalContext";
+
+// Values:
+// id --> albumID, userID
+// album --> artist, genre, id, style[], thumbnail, title, tracklist, year
+// userAlbum --> id (above), notes, status, tags[]
 
 function SavedAlbumModal() {
+    const { savedAlbumModalVisibility, toggleSavedAlbumModalVisibility,
+                userAlbum, id, album } = useSavedAlbumModalContext(); 
+
+                <> NEED TO ADD ALBUM CSS / VALUES ! MAYBE COPY OFF AlbumModal
+                    MIGHT NEED TO MOVE SOME FUNCTIONS TO A CENTRAL LOCATION i.e. 'HandleNotes' </>
 
     return (
         <>
-
+            {savedAlbumModalVisibility && (
+                <p>TESTING</p>
+            )}
         </>
     )
 }
 
-return { SavedAlbumModal };
+export { SavedAlbumModal };
