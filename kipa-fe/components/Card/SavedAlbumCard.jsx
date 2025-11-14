d2d85b4a67194041151509a5267d395d683913f5
matt <mh.dev@pm.me>
2025-11-13 23:33:24 -0500

    Added API response functionality

--- kipa-fe/components/Card/SavedAlbumCard.jsx
+++ kipa-fe/components/Card/SavedAlbumCard.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useSavedAlbumModalContext } from "../../context/SavedAlbumModalContext";
+import { useUserAuthContext } from "../../context/UserAuthContext";
+import { getUserAlbum } from "../../services/AlbumServices";
 
 
 function SavedAlbumCard({ album }) {
-    const { albumAlbumID, genre, notes, status,
+    const { albumID, genre, notes, status,
             style, tags, thumbnail, title, tracklist, year } = album;
+    const { toggleSavedAlbumModalContext, openSavedAlbumModalWithUserAlbumData } = useSavedAlbumModalContext();
+    const { username } = useUserAuthContext();
+    
     
     // Need to add the logic to cycle through all styles, genres, tags, etc..
     // Determine how tracklist will be implemented --> AlbumModal most likely? More details?
+
+
+    const handleClick = () => {
+        openSavedAlbumModalWithUserAlbumData(username, albumID);
+    }
+    
     return (
         <>
-            <div className="border-2 border-solid">
+            <div className="border-2 border-solid" onClick={handleClick}>
                 <h1>{title}</h1>
                 <img src={thumbnail}/>
                 <p>{year}</p>
