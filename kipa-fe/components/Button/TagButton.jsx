459a0333bb921995208c52fa531edf5a0f3a2cb0
matt <mh.dev@pm.me>
2025-11-19 19:18:59 -0500

    Retains default dropdown value after making selection

--- kipa-fe/components/Button/TagButton.jsx
+++ kipa-fe/components/Button/TagButton.jsx
@@ -3,9 +3,12 @@ import { useState } from "react";
 
 export default function TagButton({onSelection}) {
     const [hover, setHover] = useState(false);
+    const [selectedTag, setSelectedTag] = useState("0");
 
     const handleSelection = (e) => {
         onSelection(e.target.value);
+        setSelectedTag("0");
+        console.log("T");
     }
 
     return (
@@ -32,20 +35,22 @@ export default function TagButton({onSelection}) {
                 id="tag_dropdown_menu"
                 name="tag_dropdown_menu"
                 onChange={handleSelection}
+                value={selectedTag}
                 style={{
                     width: hover ? "130px" : "0px",
                     opacity: hover ? 1 : 0,
                     overflow: "hidden",
                     transition: "all 0.3s ease",
                     border: hover ? "1px solid #555" : "none",
-                    backgroundColor: "#3a3a3a", // Matches tracklist section
-                    color: "#d0d0d0", // Medium-light gray text
+                    backgroundColor: "#3a3a3a",
+                    color: "#d0d0d0",
                     borderRadius: "6px",
                     boxShadow: hover ? "0 2px 6px rgba(0,0,0,0.4)" : "none",
                     padding: hover ? "0.2rem" : "0",
                     fontSize: "0.9rem",
                 }}
             >
+                <option value="0" selected disabled>Select Tag:</option>
                 <option value="Hot🔥">Hot🔥</option>
                 <option value="Signed🖋️">Signed🖋️</option>
                 <option value="Limited💎">Limited💎</option>
